188101ee2fc4c8c585e2ff5622b3790fa1fa7575
Donald Propst <donald.propst@contrastsecurity.com>
2020-07-17 11:09:46 -0400

    RUBY-948 fix spelling

--- lib/vulneruby/trigger/crypto_bad_mac.rb
+++ lib/vulneruby/trigger/crypto_bad_mac.rb
@@ -5,7 +5,7 @@ module Vulneruby
     # Invoke methods which are susceptible to Crypto Bad Mac
     class CryptoBadMac
       # Run the command Digest::MD5.new to expose a vulnerable method call.
-      def self.run_digest_mdg
+      def self.run_digest_md5
         Digest::MD5.new.to_s
       end
     end
--- spec/vulneruby/trigger/crypto_bad_mac_spec.rb
+++ spec/vulneruby/trigger/crypto_bad_mac_spec.rb
@@ -6,7 +6,7 @@ describe Vulneruby::Trigger::CryptoBadMac do
   describe '.run_digest_md5' do
     it 'will run Digest::MD5.new' do
       expect(Digest::MD5).to(receive(:new))
-      described_class.run_digest_mdg
+      described_class.run_digest_md5
     end
   end
 end
